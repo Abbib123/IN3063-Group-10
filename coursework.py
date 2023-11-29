9563fabc3c7ab78363944ac2c974623a137c905d
lucy-ng <Lucy.Nguyen@city.ac.uk>
2023-11-29 15:15:21 +0000

    feat: structure for forward and backward pass

--- coursework.py
+++ coursework.py
@@ -16,16 +16,61 @@ def sigmoid(x):
 def sigmoid_derivative(x):
     return x * (1.0 - x)
 
+# ReLU
+def relu(x):
+    return np.maximum(0, x)
+
+# ReLU derivative
+def relu_derivative(x):
+    return (x >= 0) * 1
 
 # Neural Network Class
 class NeuralNetwork:
+    
+    # Initialise random weight matrices
+    w1 = np.random.random(())
+    w2 = np.random.random(())
 
     # Implement forward and backward pass for Sigmoid
-    def forward_pass_backward_pass_sigmoid(inputs, weights):
-        epochs = 10
+    def forward_pass_sigmoid(inputs, weights):
+        epochs = ...
+        
         for epoch in range(epochs):
-            np.dot()
+            z1 = np.dot()
+            a1 = sigmoid(z1)
+            
+            z2 = np.dot()
+            a2 = sigmoid(z2)
+            
+    def backward_pass_sigmoid(inputs, weights):
+        epochs = ...
+        
+        for epoch in range(epochs):
+            z1 = np.dot()
+            a1 = sigmoid_derivative(z1)
+            
+            z2 = np.dot()
+            a2 = sigmoid_derivative(z2)
     
     # Implement forward and backward pass for ReLU
+    def forward_pass_relu(inputs, weights):
+        epochs = ...
+        
+        for epoch in range(epochs):
+            z1 = np.dot()
+            a1 = relu(z1)
+            
+            z2 = np.dot()
+            a2 = relu(z2)
+            
+    def backward_pass_relu(inputs, weights):
+        epochs = ...
+        
+        for epoch in range(epochs):
+            z1 = np.dot()
+            a1 = relu_derivative(z1)
+            
+            z2 = np.dot()
+            a2 = relu_derivative(z2)
     
     # Implement softmax layer
