5d9c194fc93d467de5bf394539032f285108d3b4
Abbib123 <Abbib.Akram@city.ac.uk>
2023-12-30 19:23:40 +0000

    Update Abbib Akram-Task 1 implementation.py
    First Commit after syncing error resolved

--- Abbib Akram-Task 1 implementation.py
+++ Abbib Akram-Task 1 implementation.py
@@ -6,6 +6,19 @@ Created on Sat Dec 23 20:47:44 2023
 @author: abbibakram
 """
 
-#Testing commit
-#testing commit 2
-#testing commit 3
+from tensorflow import keras
+import numpy as np
+import random
+from keras.datsets import mnist
+import matplotlib.pyplot as plt
+
+# Reference to:https://www.analyticsvidhya.com/blog/2021/06/mnist-dataset-prediction-using-keras/#h-step1-importing-dataset
+data = mnist.load_data() 
+(X_train, y_train), (X_test, y_test) = data
+X_train[0].shape
+X_train.shape
+
+class neural_network_mnist :
+    
+    
+
