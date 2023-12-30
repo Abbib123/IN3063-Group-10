29f3d5bd7f932e3d4e16b6769585a6dbbc8a49a6
Abbib123 <Abbib.Akram@city.ac.uk>
2023-12-30 20:31:43 +0000

    Commiting first tasks

--- Abbib Akram-Task 1 implementation.py
+++ Abbib Akram-Task 1 implementation.py
@@ -18,7 +18,34 @@ data = mnist.load_data()
 X_train[0].shape
 X_train.shape
 
-class neural_network_mnist :
+class neural_network_mnist:
+    def __init__(self,inputnodes,outputnodes,hiddennodes,rate_of_learning):
+        
+        self.inputnodes = inputnodes
+        self.outputnodes = outputnodes
+        self.hiddennodes = hiddennodes
+        self.rate_of_learning = rate_of_learning
+        self.Create_weighted_nodes()
     
+   
+#Code for creating and training the neural network
+#Reference to:https://www.youtube.com/watch?v=vtx1iwmOx10
+def initialise_weights(inputnodes,hiddennodes,outputnodes):
     
-
+    h1 = np.random.randn(hiddennodes,inputnodes)*0.01
+    c1 = np.zeros((hiddennodes,1))
+    
+    h2 = np.random.randn(outputnodes,hiddennodes)
+    c2 = np.zeros((hiddennodes,1))
+    
+    weights = {
+        "h1" : h1,
+        "c1" : c1,
+        "h2" :h2,
+        "c2" : c2
+        
+        }
+    
+    return weights
+    
+        
