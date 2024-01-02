b499d7ad63445b6f9b1945ebd4d1e631d34b04e7
Abbib123 <Abbib.Akram@city.ac.uk>
2024-01-02 23:04:02 +0000

    created an instance of the neural network class

--- Abbib Akram-Task 1 implementation.py
+++ Abbib Akram-Task 1 implementation.py
@@ -16,6 +16,7 @@ from keras.datsets import mnist
 #from keras.datsets import mnist
 
 # Reference to:https://www.scaler.com/topics/deep-learning/how-to-build-a-neural-network/
+# Reference to:https://www.analyticsvidhya.com/blog/2021/06/mnist-dataset-prediction-using-keras/#h-step1-importing-dataset
 
 (X_train, y_train), (X_test, y_test) = mnist.load_data()
 
@@ -30,6 +31,11 @@ X_test /= 255
 # One-hot encode labels
 y_train = to_categorical(y_train, 8)#10
 y_test = to_categorical(y_test, 8)#10
+
+
+
+#Code for creating and training the neural network
+#Reference to:https://www.youtube.com/watch?v=vtx1iwmOx10
 
 class neural_network_mnist:
     def __init__(self,
@@ -147,16 +153,52 @@ def update_architecture(architecture,gradients,rate_of_learning):
     bias_2 = bias_2 - rate_of_learning*form_bias_2
     
     architecture = {
-        "weight_1" = weight_1,
-        "bias_1" = bias_1,
-        "weight_2"=weight_2,
-        "bias_2" = bias_2
+        "weight_1" : weight_1,
+        "bias_1" : bias_1,
+        "weight_2" : weight_2,
+        "bias_2" : bias_2
         
         }
+    
     return architecture
 
 
+
+neural_network_mnist.inputnodes = 784
+neural_network_mnist.hiddennodes = 128
+neural_network_mnist.outputnodes = 10 
+neural_network_mnist.epochs = 1000
+neural_network_mnist.rate_of_learning = 0.01 # Set to the number of classes in your problem
+
+
+def final_nn_model(inputnodes, outputnodes,hiddennodes, rate_of_learning, epochs):
     
+    inputnodes = inputnodes.shape[0]
+    outputnodes = outputnodes.shape[0]
+    
+    list_of_cost_value = []
+    
+    architecture = initialise_architecture(initialise_architecture.inputnodes,initialise_architecture.hiddennodes,
+                                           initialise_architecture.outputnodes)
+    
+    for i in range(epochs):
+        
+        forwardprop_value_storage = forward_propagation_nn(forward_propagation_nn.x, forward_propagation_nn.parameters)
+        
+        
+        cost =  cost_function_nn(forwardprop_value_storage['form_bias_2'], outputnodes)
+        
+        
+        gradients = backpropagation_nn(inputnodes, outputnodes, architecture, forwardprop_value_storage)
+        
+        architecture = update_architecture(architecture, gradients, rate_of_learning)
+        
+        list_of_cost_value.append(cost)
+        
+        if(i%(epochs/10) == 0):
+            print("Cost after", i, "iterations is :", cost)
+        
+    return architecture, cost_list
                  
     
     
@@ -188,100 +230,7 @@ def update_architecture(architecture,gradients,rate_of_learning):
 #X_train[0].shape
 #X_train.shape
 
-class neural_network_mnist:
-    def __init__(self,inputnodes,outputnodes,hiddennodes,rate_of_learning):
-        
-        self.inputnodes = inputnodes
-        self.outputnodes = outputnodes
-        self.hiddennodes = hiddennodes
-        self.rate_of_learning = rate_of_learning
-        self.Create_weighted_nodes()
-    
-   
-#Code for creating and training the neural network
-#Reference to:https://www.youtube.com/watch?v=vtx1iwmOx10
-def initialise_weights(inputnodes,hiddennodes,outputnodes):
-    
-    h1 = np.random.randn(hiddennodes,inputnodes)*0.01
-    c1 = np.zeros((hiddennodes,1))
-    
-    h2 = np.random.randn(outputnodes,hiddennodes)
-    c2 = np.zeros((hiddennodes,1))
-    
-    weights = {
-        "h1" : h1,
-        "c1" : c1,
-        "h2" :h2,
-        "c2" : c2
-        
-        }
-    
-    return weights
 
-
-    #Reference to:https://www.youtube.com/watch?v=vtx1iwmOx10
-def forward_prop_NN(inputnodes,weights):
-       
-       h1 = weights['h1']
-       c1 = weights['c1']
-       h2 = weights['h2']
-       c2 = weights['c2']
-       
-       #Matrix multiplication
-       k1=np.dot(h1,inputnodes) + c1
-       v1 = reLu(k1)  #Implementing relu later
-       
-       k2=np.dot(h2,v1)+ c2
-       v2=softMax(c2) #Implementing sigmoid later
-       
-       forward_storage = {
-           "k1" : k1,
-           "v1" : v2,
-           "k2" : k2,
-           "v2" : v2
-           
-           }  
-       return forward_storage
-       
-       
-def cost_function (v2, outputnodes):     
-    
-    m = outputnodes.shape[1]
-    
-    cost = -(1/m)*np.sum(outputnodes*np.log(v2))
-    
-    return cost
-
-def backward_prop_NN(inputnodes,outputnodes,weights,forward_storage):
-    
-    h1 = weights ['h1']
-    c1 = weights['c1']
-    h2 = weights['h2']
-    c2 = weights['c2']
-    
-    k1 = forward_storage['k1']
-    k2 = forward_storage['k2']
-    
-    m = inputnodes.shape[1]
-    
-    #Activation functions for backpropagation
-    
-    ak2 = (v2 - outputnodes)
-    ac2 = (1/m)*np.dot(ah2.T)
-    ah2 = (1/m)*np.sum(ah2,axis= 1,keepdims= 1)
-    
-   
-    
-    ah1 = (1/m)*np.dot(w2.T, ah2)*derivative_tanh(k1) #Implementing tanh later
-    ac1 = (1/m)*np.dot(ah1, x.T)
-    ah1 = (1/m)*np.sum(dz1, axis = 1, keepdims = True) 
-    
-    
-    gradients = {
-        }
-    
-    
-    
     
     
                   
