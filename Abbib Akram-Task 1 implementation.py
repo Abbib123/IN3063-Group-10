9ee6b939d22a66d04884a22fea29cda204100bd5
Abbib123 <Abbib.Akram@city.ac.uk>
2024-01-02 01:43:51 +0000

    Updated neural network class

--- Abbib Akram-Task 1 implementation.py
+++ Abbib Akram-Task 1 implementation.py
@@ -5,18 +5,149 @@ Created on Sat Dec 23 20:47:44 2023
 
 @author: abbibakram
 """
-
-from tensorflow import keras
 import numpy as np
 import random
-from keras.datsets import mnist
 import matplotlib.pyplot as plt
+import tensorflow as tf
+from tensorflow.keras.utils import to_categorical 
+from keras.datsets import mnist
+
+#from tensorflow import keras
+#from keras.datsets import mnist
+
+# Reference to:https://www.scaler.com/topics/deep-learning/how-to-build-a-neural-network/
+
+(X_train, y_train), (X_test, y_test) = mnist.load_data()
+
+X_train = X_train.reshape(60000, 784)
+X_test = X_test.reshape(10000, 784)
+X_train = X_train.astype('float32')
+X_test = X_test.astype('float32')
+X_train /= 255
+X_test /= 255
+
+
+# One-hot encode labels
+y_train = to_categorical(y_train, 8)#10
+y_test = to_categorical(y_test, 8)#10
+
+class neural_network_mnist:
+    def __init__(self,
+                 inputnodes,outputnodes,
+                 hiddennodes,function_of_activation,
+                 rate_of_learning,epochs):
+        
+        self.inputnodes = inputnodes
+        self.outputnodes = outputnodes
+        self.hiddennodes = hiddennodes
+        self.function_of_activation = function_of_activation
+        self.rate_of_learning = rate_of_learning
+        self.epochs = epochs
+        
+def initialise_architecture(self,inputnodes,outputnodes,hiddennodes):
+    
+    self.weight_1 = np.random.randint(inputnodes,hiddennodes)*0.01#This is to make the terms smaller
+    self.bias_1 = np.zeroes((hiddennodes,1))
+    self.weight_2 = np.random.randint(outputnodes,hiddennodes)*0.01
+    self.bias_2 = np.zeroes((outputnodes,1))
+    
+    architecture = {
+        
+        "weight_1":self.weight_1,
+        
+        "bias_1":self.bias_1,
+        
+        "weight_2":self.weight_2,
+        
+        "bias_2":self.bias_2
+        
+        }
+    return architecture
+
+def forward_propagation_nn(inputnodes,architecture):
+    
+    initialise_architecture.weight_1 = architecture ['weight_1']
+    initialise_architecture.bias_1 = architecture['bias_1']
+    initialise_architecture.weight_2 = architecture['weight_2']
+    initialise_architecture.bias_1 = architecture['bias_2']
+    
+    
+    form_weight_1 = np.dot(initialise_architecture.weight_1,inputnodes + initialise_architecture.bias_1)
+    
+    form_bias_1 = tanh(form_weight_1)
+    
+    form_weight_2 = np.dot(initialise_architecture.weight_2,form_bias_1)+initialise_architecture.bias1
+                           
+    form_bias_2 = softMax(form_weight_2)
+    
+    
+    forwardprop_value_storage = {
+        "form_weight_1":form_weight_1,
+        "form_bias_1 ":form_bias_2,
+        "form_weight_2":form_weight_2,
+        "form_bias_2":form_bias_2
+        
+        }
+    return forwardprop_value_storage
+
+def cost_function_nn(form_bias_2,outputnodes):
+    
+    h = outputnodes.shape[1]
+    
+    cost = -(1/h)*np.sum(outputnodes*np.log(form_bias_2))
+    
+    return cost
+
+def backpropagation_nn(inputnodes,outputnodes,architecture,forwardprop_value_storage):
+    
+    initialise_architecture.weight_1 = architecture ['weight_1']
+    initialise_architecture.bias_1 = architecture ['bias_1']
+    initialise_architecture.weight_2 = architecture ['weight_2']
+    initialise_architecture.bias_2 = architecture ['bias_2']
+    
+    form_bias_1 = forwardprop_value_storage ['form_bias_1']
+    form_bias_2 = forward_propagation_nn ['form_bias_2']
+    
+    h = inputnodes.shape[1]
+    
+    form_com2 = (form_bias_2 - outputnodes)
+    form_weight_2 = (1/h)*np.dot(form_com2,form_bias_1)
+    form_bias_2 = (1/h)*np.sum(form_com2,axis=1,keepdims=True)
+    
+    form_com1 = (1/h)*np.dot(initialise_architecture.weight_2.T,form_com2)*derivative_tanh
+    
+    
+    form_weight_1 = (1/h)*np.dot(initialise_architecture.weight_2.form_com2)*derivative_tanh
+                 
+    
+    
+    
+    
+    
+    
+                           
+                           
+    
+    
+    
+    
+        
+        
+
+        
+        
+    
+
+
+
+
+
 
 # Reference to:https://www.analyticsvidhya.com/blog/2021/06/mnist-dataset-prediction-using-keras/#h-step1-importing-dataset
-data = mnist.load_data() 
-(X_train, y_train), (X_test, y_test) = data
-X_train[0].shape
-X_train.shape
+#data = mnist.load_data() 
+#(X_train, y_train), (X_test, y_test) = data
+#X_train[0].shape
+#X_train.shape
 
 class neural_network_mnist:
     def __init__(self,inputnodes,outputnodes,hiddennodes,rate_of_learning):
